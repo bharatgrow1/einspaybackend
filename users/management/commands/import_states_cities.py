fe2458eaa675b52cea5810cf2ff9b1835c664b68
pritesh <pritesh@kali.pritesh>
2026-01-06 14:29:36 +0530

    app updated

--- users/management/commands/import_states_cities.py
+++ users/management/commands/import_states_cities.py
@@ -6,14 +6,21 @@ class Command(BaseCommand):
     help = "Import Indian States and Cities from Excel"
 
     def handle(self, *args, **kwargs):
-        file_path = "india_states_cities.xlsx"  # project root me rakho
+        file_path = "india_states_cities.xlsx"
 
         df = pd.read_excel(file_path)
 
+        df.columns = df.columns.str.strip().str.lower()
+
         for _, row in df.iterrows():
-            state_name = row["state"].strip()
-            state_code = row["state_code"].strip()
-            city_name = row["city"].strip()
+
+            if pd.isna(row.get("state")) or pd.isna(row.get("city")):
+                continue
+
+            state_name = str(row["state"]).strip()
+            state_code = str(row["state_code"]).strip()
+            city_name = str(row["city"]).strip()
+            district_code = str(row["district_code"]).strip()
 
             state, _ = State.objects.get_or_create(
                 name=state_name,
@@ -22,7 +29,12 @@ class Command(BaseCommand):
 
             City.objects.get_or_create(
                 state=state,
-                name=city_name
+                name=city_name,
+                defaults={
+                    "district_code": district_code
+                }
             )
 
-        self.stdout.write(self.style.SUCCESS("✅ States & Cities imported successfully"))
+        self.stdout.write(
+            self.style.SUCCESS(" States, Cities & District Codes imported successfully")
+        )
